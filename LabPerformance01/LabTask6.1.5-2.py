b47b91aba5435e504dde6c75a4ac969e3a53735d
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2025-02-03 23:15:25 +0600

    Smallest Number from a Set

---
+++ LabPerformance01/LabTask6.1.5-2.py
@@ -0,0 +1,7 @@
+# • Write a python program to find the smallest number from a set of numbers.
+
+num = [5, 3, 9, 1, 7]
+for x in num:
+    smallest = min(num)
+    print("Smallest number:", smallest)
+    break;
